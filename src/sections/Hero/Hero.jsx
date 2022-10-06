f93d242011048105e47ac3074aace4d5b2f35015
unknown <calebjanhunen@gmail.com>
2022-10-06 16:58:46 -0400

    Added animations in hero section

--- src/sections/Hero/Hero.jsx
+++ src/sections/Hero/Hero.jsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useRef } from 'react';
 import { FaGithubSquare, FaLinkedin } from 'react-icons/fa';
 
 import './Hero.scss';
@@ -6,16 +6,36 @@ import './Hero.scss';
 import picture from '../../images/hero-img.png';
 
 const Hero = () => {
+    const linkedinRef = useRef();
+    const githubRef = useRef();
+
+    //removes animation class after animation is done
+    setTimeout(() => {
+        linkedinRef.current?.classList.remove('social-link--expand');
+        githubRef.current?.classList.remove('social-link--expand');
+    }, 1000);
+
     return (
         <section className="section-hero" id="hero">
             <div className="hero">
                 <img className="hero__img" src={picture} alt="" />
                 <div className="hero__text">
-                    <h1 className="heading-primary">
+                    <h1 className="heading-primary hero__heading">
                         Hi, I'm{' '}
                         <span className="hero__name">Caleb Janhunen</span>
                     </h1>
-                    <p className="hero__subtitle">A Full Stack Developer</p>
+                    <p className="hero__subtitle">
+                        A{' '}
+                        <span className="hero__rotating-text">
+                            Computer Engineer
+                        </span>{' '}
+                        <span className="hero__rotating-text">
+                            Full Stack Developer
+                        </span>{' '}
+                        <span className="hero__rotating-text">
+                            Problem Solver
+                        </span>
+                    </p>
 
                     <a href="#contact-form" className="btn hero__cta">
                         Get in touch
@@ -26,8 +46,9 @@ const Hero = () => {
                             <a
                                 href="https://www.linkedin.com/in/caleb-janhunen-362b62171/"
                                 target="_blank"
-                                className="social-link"
+                                className="social-link social-link--expand"
                                 rel="noreferrer"
+                                ref={linkedinRef}
                             >
                                 <FaLinkedin size={40} />
                             </a>
@@ -36,8 +57,9 @@ const Hero = () => {
                             <a
                                 href="https://github.com/calebjanhunen"
                                 target="_blank"
-                                className="social-link"
+                                className="social-link social-link--expand"
                                 rel="noreferrer"
+                                ref={githubRef}
                             >
                                 <FaGithubSquare size={40} />
                             </a>
