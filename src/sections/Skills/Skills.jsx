185f9a9209e959fa7fbe5320e3ac5f01d1c393c5
calebjanhunen <calebjanhunen@gmail.com>
2023-12-21 16:50:59 -0500

    Added skills

--- src/sections/Skills/Skills.jsx
+++ src/sections/Skills/Skills.jsx
@@ -6,6 +6,9 @@ import cssLogo from '../../images/logos/css3.svg';
 import htmlLogo from '../../images/logos/html.svg';
 import javaLogo from '../../images/logos/java.svg';
 import javascriptLogo from '../../images/logos/javascript.svg';
+import typescriptLogo from '../../images/logos/typescript.svg';
+import phpLogo from '../../images/logos/php.svg';
+
 
 // Frameworks & Libraries logos
 import expressLogo from '../../images/logos/express.svg';
@@ -13,6 +16,8 @@ import mongodbLogo from '../../images/logos/mongodb.svg';
 import nodeLogo from '../../images/logos/node-js.svg';
 import reactLogo from '../../images/logos/react.svg';
 import socketioLogo from '../../images/logos/Socket-io.svg';
+import nestjsLogo from '../../images/logos/nestjs.svg';
+import jestLogo from '../../images/logos/jest.svg';
 
 // Technologies Logos
 import gitLogo from '../../images/logos/git.svg';
@@ -21,7 +26,9 @@ import npmLogo from '../../images/logos/npm.svg';
 import postmanLogo from '../../images/logos/postman.svg';
 import restApiLogo from '../../images/logos/rest-api-icon.webp';
 import vsCodeLogo from '../../images/logos/vs-code.svg';
-import windowsLogo from '../../images/logos/windows.svg';
+import dockerLogo from '../../images/logos/docker.svg';
+import postgresLogo from '../../images/logos/postgresql.svg';
+import jiraLogo from '../../images/logos/jira.svg';
 
 import './Skills.scss';
 
@@ -31,16 +38,20 @@ import SkillContainer from './SkillContainer/SkillContainer';
 const Skills = () => {
     const languagesLogos = [
         { img: javascriptLogo, alt: 'JavaScript Logo' },
+        { img: typescriptLogo, alt: 'Typescript Logo' },
         { img: htmlLogo, alt: 'HTML Logo' },
         { img: cssLogo, alt: 'CSS Logo' },
         { img: javaLogo, alt: 'Java Logo' },
         { img: cLogo, alt: 'C Logo' },
+        { img: phpLogo, alt: 'PHP Logo' },
     ];
 
     //nodejs, expressjs, mongodb,  sass?
     const frameworksLogos = [
         { img: reactLogo, alt: 'React Logo' },
         { img: nodeLogo, alt: 'NodeJS Logo' },
+        { img: nestjsLogo, alt: 'NestJS Logo' },
+        { img: jestLogo, alt: 'Jest Logo' },
         { img: expressLogo, alt: 'ExpressJS Logo' },
         { img: mongodbLogo, alt: 'MongoDB Logo' },
         { img: socketioLogo, alt: 'SocketIO Logo' },
@@ -49,10 +60,12 @@ const Skills = () => {
     //postman, linux, npm, vscode, windows, rest api
     const techLogos = [
         { img: gitLogo, alt: 'Git Logo' },
+        { img: jiraLogo, alt: 'Jira Logo' },
         { img: postmanLogo, alt: 'Postman Logo' },
         { img: npmLogo, alt: 'npm Logo' },
+        { img: postgresLogo, alt: 'PostgreSQL Logo' },
         { img: linuxLogo, alt: 'Linux Logo' },
-        { img: windowsLogo, alt: 'Windows Logo' },
+        { img: dockerLogo, alt: 'Docker Logo' },
         { img: vsCodeLogo, alt: 'vsCode Logo' },
         { img: restApiLogo, alt: 'RestAPI Logo' },
     ];
